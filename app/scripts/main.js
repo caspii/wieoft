70ea384fcdc9c8b17efc7de9078b9989fe75079a
caspii <caspar.wrede@gmail.com>
2014-11-30 23:57:55 +0100

    JQuery animation working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+function doAnimate(el) {
+	setInterval(function() {animate(el)},1000);
+} 
+
+function animate(el) {
+	el.toggleClass("pulse");
+}
+
+doAnimate($(".animated-tile"));
