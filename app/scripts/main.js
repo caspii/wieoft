303a69bebfee743dd2350675a1478d6b3129b229
caspii <caspar.wrede@gmail.com>
2014-12-06 18:29:29 +0100

    Switched to frequency based time information

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,24 @@
 function doAnimate(el) {
-		delay = el.data('delay');
+
+		frequency = el.data('frequency'); 	// Frequency per minute
+		
+
+		if (frequency > 60) {
+			// Envent happens more than once a second, animation duration length must shortened
+			duration = (60/frequency)*1000 / 2;
+			delay = duration;
+		} else {
+			duration = 800;
+			delay = (60/frequency)*1000 - duration; 
+
+		}
+
 		console.log("Delay = " + delay);
 		// Add delay to el
 		setInterval(
 			function() {
-				el.toggleClass("pulse");
+				el.toggleClass('pulse');
+				el.css('animation-duration', duration + 'ms')
 			},delay);	
 };
 	
