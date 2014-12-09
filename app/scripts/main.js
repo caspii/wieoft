eb06df86a778c5054e2747c8f29112c2098e3c0d
caspii <caspar.wrede@gmail.com>
2014-12-09 08:08:26 +0100

    Added logging

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,22 +3,25 @@ function doAnimate(el) {
 		frequency = el.data('frequency'); 	// Frequency per minute
 		
 
-		if (frequency > 60) {
-			// Envent happens more than once a second, animation duration length must shortened
+		if ((60/frequency*1000) > 800) {
+			// Event happens more than once a second, animation duration length must shortened
 			duration = (60/frequency)*1000 / 2;
 			delay = duration;
 		} else {
 			duration = 800;
-			delay = (60/frequency)*1000 - duration; 
+			delay = (60/frequency)*1000 - duration;
 
 		}
 
-		console.log("Delay = " + delay);
+		console.log(el.html() + 
+			' Delay = ' + delay +
+			' duration = ' + duration +
+			' total = ' + (duration + delay));
 		// Add delay to el
 		setInterval(
 			function() {
-				el.toggleClass('pulse');
 				el.css('animation-duration', duration + 'ms')
+				el.toggleClass('pulse');
 			},delay);	
 };
 	
