5f1dd78c019a3f10fd14d9390e4a195e36ad900b
caspii <caspar.wrede@gmail.com>
2015-01-17 11:28:04 +0100

    Auto committed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ function doAnimate(el) {
 		frequency = el.data('frequency'); 	// Frequency per minute
 		
 		total_duration = (60/frequency)*1000;
-		animation_duration = 800;
+		animation_duration = 100;
 
 		if (total_duration < 800) {
 			// total_duration too short, animation must be shortened too.
@@ -12,15 +12,15 @@ function doAnimate(el) {
 		} 
 
 		console.log(el.html() + 
-			' total_duration = ' + total_duration +
-			' animation_duration = ' + animation_duration);
+			' animation_duration = ' + animation_duration +
+			' total_duration = ' + total_duration );
 
 		// Add total_duration to el
 		setInterval(
 			function() {
-				el.css('animation-duration', animation_duration + 'ms')
+				el.css('animation-duration', animation_duration + 'ms');
 				el.toggleClass('pulse');
-			},total_duration);	
+			},total_duration);
 };
 	
 	
