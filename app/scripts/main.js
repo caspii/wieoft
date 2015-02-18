6f10ce774451e1defb4e8bfd3d69c80d5487d2c6
caspii <caspar.wrede@gmail.com>
2015-02-18 08:50:18 +0100

    Animation, next milestone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,23 @@
 
-$.keyframe.define([{
-    name: 'trapdoor-sequence',
-    '0%': {'opacity': '1'},
-    '60%': {'opacity': '0.3'},
-    '90%': {'opacity': '0.3'},
-    '100%': {'opacity': '1'}
-}]);
-
-
-
 
 function doAnimate(el) {
-$(el).playKeyframe({
-    name: 'trapdoor-sequence',
-    duration: '1s',
-    iterationCount: 'infinite',
+	var animationDuration = 500;				// ms		
+	var frequency = el.data('frequency'); 		// Frequency per minute
+	var totalDuration = (60/frequency)*1000; 	// ms
+	var interval = totalDuration - animationDuration;
+
+
+	$.keyframe.define([{
+    	name: 'pulse',
+    		'0%': {'opacity': '0.3'},
+    		'30%': {'opacity': '1'},
+    		'100%': {'opacity': '0.3'}
+	}]);
+
+	$(el).playKeyframe({
+    	name: 'pulse',
+    	duration: totalDuration + "ms",
+    	iterationCount: 'infinite',
     });
 }
 	
