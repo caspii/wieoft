43edaae03b40f07a6eedaaf60934103c33fbb525
caspii <caspar.wrede@gmail.com>
2015-02-18 07:38:48 +0100

    First working version of Keyframes animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,21 @@
-// Properties for the pulse
-var properties = {
-    opacity:1,
-    color: "white",
-	backgroundColor: "background:rgba(255,255,255, 0.3);"
-};
 
-// On devices more than 788px wide, remove expensive background animation to tiles
-var windowWidth = window.screen.width < window.outerWidth ? 
-window.screen.width : window.outerWidth;
-if ( windowWidth < 768) {
-	properties.backgroundColor = "";
-} 
+$.keyframe.define([{
+    name: 'trapdoor-sequence',
+    '0%': {'opacity': '1'},
+    '60%': {'opacity': '0.3'},
+    '90%': {'opacity': '0.3'},
+    '100%': {'opacity': '1'}
+}]);
+
+
 
 
 function doAnimate(el) {
-
-		var animationDuration = 500;				// ms		
-		var frequency = el.data('frequency'); 		// Frequency per minute
-		var totalDuration = (60/frequency)*1000; 	// ms
-		var interval = totalDuration - animationDuration;
-
-		if (interval < 1 ) {
-			// totalDuration too short, animation must be shortened too.
-			animationDuration = totalDuration / 2;
-			interval = totalDuration / 2;
-		} 
-
-		el.pulse(properties,
-			{ 
-				duration: animationDuration, 
-				interval : interval,
-				pulses:-1
-			});
+$(el).playKeyframe({
+    name: 'trapdoor-sequence',
+    duration: '1s',
+    iterationCount: 'infinite',
+    });
 }
 	
 	
